23708ecc5470e3b2cf0ac5f0466549d781469ddb
mariahMurdock <mariahmurdock1227@gmail.com>
2021-01-26 20:45:32 -0800

    bring project up to date

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+// import { Link } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <>
-            <nav className="navbar">
-               <div className="navbar-container">
-                   <Link to="/" className="navbar-logo">
-                       TRVL 
-                   </Link>
+            <nav className='navbar'>
+               <div className='navbar-container'>
+                   <BrowserRouter>
+                    <Link to='/' className='navbar-logo'>
+                        TRVL <i className='fab fa-typo3'/>
+                    </Link>
+                   </BrowserRouter>
                </div>
             </nav> 
         </>
